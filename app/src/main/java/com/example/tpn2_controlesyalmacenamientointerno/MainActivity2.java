7ca9fec9f61abba89753c04323b323d6964951a7
JRodriguez-1998 <jose.rodriguez@alumnos.frgp.utn.edu.ar>
2021-09-03 00:51:36 -0300

    Le agregué al Segundo Activity el Menú que me faltó.

--- app/src/main/java/com/example/tpn2_controlesyalmacenamientointerno/MainActivity2.java
+++ app/src/main/java/com/example/tpn2_controlesyalmacenamientointerno/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.tpn2_controlesyalmacenamientointerno;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu m){
+        getMenuInflater().inflate(R.menu.menu_activity,m);
+        return true;
     }
 }
