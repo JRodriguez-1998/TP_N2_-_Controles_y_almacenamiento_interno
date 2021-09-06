40e4c820d34154476fdd503d9e4fa55576323000
Juan <40309961+JuanGonzalez99@users.noreply.github.com>
2021-09-05 23:18:34 -0300

    guardar archivo

--- app/src/main/java/com/example/tpn2_controlesyalmacenamientointerno/MainActivity2.java
+++ app/src/main/java/com/example/tpn2_controlesyalmacenamientointerno/MainActivity2.java
@@ -2,11 +2,27 @@ package com.example.tpn2_controlesyalmacenamientointerno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.Switch;
+import android.widget.Toast;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -40,5 +56,47 @@ public class MainActivity2 extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu m){
         getMenuInflater().inflate(R.menu.menu_activity,m);
         return true;
+    }
+
+    public void Guardar(View view) {
+        if (r1.isChecked())
+            contacto.setNivelEstudios(r1.getText().toString());
+        else if (r2.isChecked())
+            contacto.setNivelEstudios(r2.getText().toString());
+        else if (r3.isChecked())
+            contacto.setNivelEstudios(r3.getText().toString());
+        else if (r4.isChecked())
+            contacto.setNivelEstudios(r4.getText().toString());
+        else if (r5.isChecked())
+            contacto.setNivelEstudios(r5.getText().toString());
+        else
+        {
+            Toast.makeText(this, "Debe seleccionar un nivel de estudios", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        List<String> intereses = new ArrayList<String>();
+        if (chk1.isChecked())
+            intereses.add(chk1.getText().toString());
+        if (chk2.isChecked())
+            intereses.add(chk2.getText().toString());
+        if (chk3.isChecked())
+            intereses.add(chk3.getText().toString());
+        if (chk4.isChecked())
+            intereses.add(chk4.getText().toString());
+        contacto.setIntereses(intereses);
+
+        contacto.setRecibirInformacion(sw.isChecked());
+
+        try {
+            ObjectOutputStream objOutput = new ObjectOutputStream(openFileOutput("contactos.dat", MODE_PRIVATE));
+            objOutput.writeObject(contacto);
+            objOutput.close();
+
+            Toast.makeText(this, "El contacto se ha guardado correctamente.", Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception ex) {
+            Toast.makeText(this, "Ha ocurrido un error al guardar el contacto.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
